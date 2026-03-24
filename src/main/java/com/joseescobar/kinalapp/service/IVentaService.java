f9ea3cfd121ddd2aa5ce3fb515a44be13d0841f1
jescobar-2025070 <jescobar-2025070@kinal.edu.gt>
2026-03-24 17:56:06 -0600

    feat: crea interfaz IVentaRepository extendiendo JpaRepository

--- src/main/java/com/joseescobar/kinalapp/service/IVentaService.java
+++ src/main/java/com/joseescobar/kinalapp/service/IVentaService.java
@@ -1,4 +1,16 @@
 package com.joseescobar.kinalapp.service;
 
+
+import com.joseescobar.kinalapp.entity.Venta;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IVentaService {
+    List<Venta> listarTodos();
+    Venta guardar(Venta venta);
+    Optional<Venta> buscarPorCodigo(int codigoVenta);
+    Venta actualizar(int codigoVenta, Venta venta);
+    void eliminar(int codigoVenta);
+    boolean existePorCodigo(int codigoVenta);
 }
