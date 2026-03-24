5e58e2ca03c78c883678519a4f063d05a102b173
jescobar-2025070 <jescobar-2025070@kinal.edu.gt>
2026-03-24 17:52:10 -0600

    feat: implementa la entidad Venta con JPA

--- src/main/java/com/joseescobar/kinalapp/entity/Venta.java
+++ src/main/java/com/joseescobar/kinalapp/entity/Venta.java
@@ -1,4 +1,55 @@
 package com.joseescobar.kinalapp.entity;
 
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name="Ventas")
 public class Venta {
+
+    @Id
+    @Column(name="codigo_venta")
+    private int codigoVenta;
+    @Column(name="fecha_venta")
+    private LocalDate fechaVenta;
+    @Column
+    private double total;
+    @Column
+    private int estado;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="Usuarios_codigo_usuario", nullable = false)
+    private Usuario usuario;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="Clientes_dpi_cliente", nullable = false)
+    private Cliente cliente;
+
+    public Venta() {
+    }
+
+    public Venta(int codigoVenta, LocalDate fechaVenta, double total, int estado, Usuario usuario, Cliente cliente) {
+        this.codigoVenta = codigoVenta;
+        this.fechaVenta = fechaVenta;
+        this.total = total;
+        this.estado = estado;
+        this.usuario = usuario;
+        this.cliente = cliente;
+    }
+
+    public int getCodigoVenta() { return codigoVenta; }
+    public void setCodigoVenta(int codigoVenta) { this.codigoVenta = codigoVenta; }
+
+    public LocalDate getFechaVenta() { return fechaVenta; }
+    public void setFechaVenta(LocalDate fechaVenta) { this.fechaVenta = fechaVenta; }
+
+    public double getTotal() { return total; }
+    public void setTotal(double total) { this.total = total; }
+
+    public int getEstado() { return estado; }
+    public void setEstado(int estado) { this.estado = estado; }
+
+    public Usuario getUsuario() { return usuario; }
+    public void setUsuario(Usuario usuario) { this.usuario = usuario; }
+
+    public Cliente getCliente() { return cliente; }
+    public void setCliente(Cliente cliente) { this.cliente = cliente; }
 }
