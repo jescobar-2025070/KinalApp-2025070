de60ab5da13dac4519aaba92c64712e7a6f28f1a
jescobar-2025070 <jescobar-2025070@kinal.edu.gt>
2026-03-24 11:50:36 -0600

    feat: Se agrega la interfaz de UsuarioService

--- src/main/java/com/joseescobar/kinalapp/service/IUsuarioService.java
+++ src/main/java/com/joseescobar/kinalapp/service/IUsuarioService.java
@@ -1,4 +1,16 @@
 package com.joseescobar.kinalapp.service;
 
+import com.joseescobar.kinalapp.entity.Usuario;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IUsuarioService {
+    List<Usuario> listarTodos();
+    Usuario guardar(Usuario usuario);
+    Optional<Usuario> buscarPorCodigo(int codigoUsuario);
+    Usuario actualizar(int codigoUsuario, Usuario usuario);
+    void eliminar(int codigoUsuario);
+    boolean existePorCodigo(int codigoUsuario);
+    List<Usuario> findByEstado(int estado);
 }
