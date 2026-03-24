de1f7c81124246de62bfe3e356a034b096164169
jescobar-2025070 <jescobar-2025070@kinal.edu.gt>
2026-03-24 11:54:55 -0600

    feat: Se agrega UsuarioService y UsuarioController parcialmente trabajados

--- src/main/java/com/joseescobar/kinalapp/controller/UsuarioController.java
+++ src/main/java/com/joseescobar/kinalapp/controller/UsuarioController.java
@@ -1,4 +1,10 @@
 package com.joseescobar.kinalapp.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
+
 }
--- src/main/java/com/joseescobar/kinalapp/service/UsuarioService.java
+++ src/main/java/com/joseescobar/kinalapp/service/UsuarioService.java
@@ -1,4 +1,71 @@
 package com.joseescobar.kinalapp.service;
 
-public class UsuarioService {
+import com.joseescobar.kinalapp.entity.Cliente;
+import com.joseescobar.kinalapp.entity.Usuario;
+import com.joseescobar.kinalapp.repository.UsuarioRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class UsuarioService implements IUsuarioService{
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public List<Usuario> listarTodos() {
+        return usuarioRepository.findAll();
+    }
+
+    @Override
+    public Usuario guardar(Usuario usuario) {
+        validarUsuario(usuario);
+         if(usuario.getEstado() == 0) {
+             usuario.setEstado(1);
+         }
+         return usuarioRepository.save(usuario);
+    }
+
+    @Override
+    public Optional<Usuario> buscarPorCodigo(int codigoUsuario) {
+        return usuarioRepository.findById(String.valueOf(codigoUsuario));
+    }
+
+    @Override
+    public Usuario actualizar(int codigoUsuario, Usuario usuario) {
+        return null;
+    }
+
+    @Override
+    public void eliminar(int codigoUsuario) {
+
+    }
+
+    @Override
+    public boolean existePorCodigo(int codigoUsuario) {
+        return false;
+    }
+
+    @Override
+    public List<Usuario> findByEstado(int estado) {
+        return List.of();
+    }
+
+    private void validarUsuario(Usuario usuario){
+        if(usuario.getCodigoUsuario() !=0 ) {
+            throw new IllegalArgumentException("El codigo es un obligatorio");
+        }
+        if(usuario.getUserName() == null || usuario.getUserName().trim().isEmpty()){
+            throw new IllegalArgumentException("El usuario es obligatorio");
+        }
+        if(usuario.getPassword() == null || usuario.getPassword().trim().isEmpty()){
+            throw new IllegalArgumentException("El password es obligatorio");
+        }
+    }
 }
