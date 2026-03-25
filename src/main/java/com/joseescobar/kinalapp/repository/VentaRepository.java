8615094749ac210bfe73233dd2cf663d027cd984
jescobar-2025070 <jescobar-2025070@kinal.edu.gt>
2026-03-25 08:43:18 -0600

    feat: agrega listar por estado en VentaRepository

--- src/main/java/com/joseescobar/kinalapp/repository/VentaRepository.java
+++ src/main/java/com/joseescobar/kinalapp/repository/VentaRepository.java
@@ -3,5 +3,8 @@ package com.joseescobar.kinalapp.repository;
 import com.joseescobar.kinalapp.entity.Venta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface VentaRepository extends JpaRepository<Venta, Integer> {
+    List<Venta> findByEstado(int estado);
 }
