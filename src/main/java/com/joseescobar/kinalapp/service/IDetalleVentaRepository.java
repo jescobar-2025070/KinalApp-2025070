a02c8393c3edfc19ed2b1d615885aaab5c2293a3
jescobar-2025070 <jescobar-2025070@kinal.edu.gt>
2026-03-25 11:20:11 -0600

    feat: implementa interfaz IDetalleVentaRepository extendiendo JpaRepository

--- src/main/java/com/joseescobar/kinalapp/service/IDetalleVentaRepository.java
+++ src/main/java/com/joseescobar/kinalapp/service/IDetalleVentaRepository.java
@@ -1,4 +1,13 @@
 package com.joseescobar.kinalapp.service;
 
+import com.joseescobar.kinalapp.entity.DetalleVenta;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IDetalleVentaRepository {
+    List<DetalleVenta> listarTodos();
+    DetalleVenta guardar(DetalleVenta detalleVenta);
+    Optional<DetalleVenta> buscarPorCodigo(int codigoDetalleVenta);
+
 }
