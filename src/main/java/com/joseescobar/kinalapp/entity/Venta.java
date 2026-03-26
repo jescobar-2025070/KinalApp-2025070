80398b1b73ca600b679b40d4fe231883e549696e
jescobar-2025070 <jescobar-2025070@kinal.edu.gt>
2026-03-25 19:29:31 -0600

    fix: corrige el tipo de dato en total de double a BigDecimal

--- src/main/java/com/joseescobar/kinalapp/entity/Venta.java
+++ src/main/java/com/joseescobar/kinalapp/entity/Venta.java
@@ -1,6 +1,7 @@
 package com.joseescobar.kinalapp.entity;
 
 import jakarta.persistence.*;
+import java.math.BigDecimal; // Importación necesaria
 import java.time.LocalDate;
 
 @Entity
@@ -12,13 +13,14 @@ public class Venta {
     private int codigoVenta;
     @Column(name="fecha_venta")
     private LocalDate fechaVenta;
-    @Column
-    private double total;
+    @Column(precision = 10, scale = 2)
+    private BigDecimal total;
     @Column
     private int estado;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="Usuarios_codigo_usuario", nullable = false)
     private Usuario usuario;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="Clientes_dpi_cliente", nullable = false)
     private Cliente cliente;
@@ -26,7 +28,7 @@ public class Venta {
     public Venta() {
     }
 
-    public Venta(int codigoVenta, LocalDate fechaVenta, double total, int estado, Usuario usuario, Cliente cliente) {
+    public Venta(int codigoVenta, LocalDate fechaVenta, BigDecimal total, int estado, Usuario usuario, Cliente cliente) {
         this.codigoVenta = codigoVenta;
         this.fechaVenta = fechaVenta;
         this.total = total;
@@ -41,8 +43,8 @@ public class Venta {
     public LocalDate getFechaVenta() { return fechaVenta; }
     public void setFechaVenta(LocalDate fechaVenta) { this.fechaVenta = fechaVenta; }
 
-    public double getTotal() { return total; }
-    public void setTotal(double total) { this.total = total; }
+    public BigDecimal getTotal() { return total; }
+    public void setTotal(BigDecimal total) { this.total = total; }
 
     public int getEstado() { return estado; }
     public void setEstado(int estado) { this.estado = estado; }
