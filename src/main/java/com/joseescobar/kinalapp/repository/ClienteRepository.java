d52d3b8042e5591e03918a03055574142c2e0324
jescobar-2025070 <jescobar-2025070@kinal.edu.gt>
2026-03-27 15:47:38 -0600

    fix: corrige el tipo de dato de int a Long en ClienteRepository

--- src/main/java/com/joseescobar/kinalapp/repository/ClienteRepository.java
+++ src/main/java/com/joseescobar/kinalapp/repository/ClienteRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ClienteRepository extends JpaRepository<Cliente, String> {
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
     List<Cliente> findByEstado(int estado);
 }
